7d3eb79ffeebd1e51b672acdc76de1ae12cfde37
tanvirhasan91 <163386945+tanvirhasan91@users.noreply.github.com>
2026-01-13 00:00:28 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,116 @@
+লিংক  https://www.facebook.com/ShekhMahmud.24
+
+Sagor Khan
+11:24 PM
+ভাই
+
+Resell Kori
+11:27 PM
+https://chatgpt.com/backend-api/estuary/content?id=file_00000000c9b471faa431b6c2f78eba94&ts=490673&p=fs&cid=1&sig=6839b05d07dcb5893f316ae4313754e5f5c9a272b99223fec176165b2c3ed619&v=0
+
+Ashraful Alam
+11:30 PM
+Gmini deya o kora jai
+
+Resell Kori
+11:30 PM
+এই লিংক দেখলেও হবে জারা উপরের লিংক দেখতে পারতেছেন না
+https://resellkori.com/product/test-2/
+যাস্ট ছবিটা দেখুন তাহলেই হবে
+
+Maruf Ahmed
+11:51 PM
+আমি
+
+Metro Maa
+11:52 PM
+1 lakh
+
+murshad
+11:52 PM
+https://www.facebook.com/themobilemedia.collection/
+
+Metro Maa
+11:52 PM
+Gift item
+
+Maruf Ahmed
+11:53 PM
+১০০০০
+
+Mdmosharof Hossain
+11:53 PM
+https://www.facebook.com/mkonlineshopbdcom
+
+Joe Shariar
+11:53 PM
+https://www.facebook.com/muslimhouse101
+
+murshad
+11:53 PM
+https://www.facebook.com/MehekeNoor/
+
+Maruf Ahmed
+11:53 PM
+https://www.facebook.com/share/17ozwVygch/
+
+erfat jahan
+11:53 PM
+https://www.facebook.com/share/15mgnRhRSy/
+
+Ashraful Alam
+11:54 PM
+https://web.facebook.com/profile.php?id=61585144423096
+
+Md. Al-Amin
+11:54 PM
+https://www.facebook.com/paikaridokandhaka/
+
+Easin Shekh
+11:54 PM
+ami ai platform e kaj korte Chai
+
+Sohel Mart BD
+11:55 PM
+https://www.facebook.com/profile.php?id=61580152350402
+
+Metro Maa
+11:55 PM
+2 page  miliye
+https://www.facebook.com/MetroMaaofc
+https://www.facebook.com/DakbaxoOfficial
+
+unityebazar
+11:56 PM
+https://www.facebook.com/unityebazar/
+
+TUSHAR AHAMMED
+11:56 PM
+https://www.facebook.com/tushartechbd
+keep
+Pinned
+
+erfat jahan
+12:02 AM
+https://www.facebook.com/share/17jLF4FBuo/
+
+erfat jahan
+12:04 AM
+https://www.facebook.com/share/p/1GCgZZ1J5G/
+
+Sohel Mart BD
+12:12 AM
+https://www.facebook.com/profile.php?id=61580152350402
+
+Resell Kori
+12:14 AM
+https://openzpf.com/product/%e0%a7%a7%e0%a7%a6-%e0%a6%aa%e0%a6%bf%e0%a6%b8-%e0%a6%9f%e0%a7%87%e0%a6%b8%e0%a7%8d%e0%a6%9f%e0%a6%bf%e0%a6%82-%e0%a6%b8%e0%a7%8d%e0%a6%aa%e0%a7%8d%e0%a6%b0%e0%a7%87-%e0%a6%aa%e0%a6%be%e0%a6%b0/
+
+Md. Zobaer Rashid
+12:14 AM
+https://www.facebook.com/share/17euFGW69S/?mibextid=wwXIfr
+
+Shekh Mahmudur Rahman
+12:19 AM
+আলহামদুলিল্লাহ আমি গ্রাফিক্স ডিজাইনার। রাসেল ভাইয়া যে ডিজাইন টা করেছেন, সেটা আমার করতে ১০ মিনিট লাগবে। যদি কারো প্রয়োজন পরে আমাকে বলতে পারেন, ইলাস্ট্রেটর ফাইল দিয়ে দিব ইনশাআল্লাহ। ফেইসবুক লিংক  https://www.facebook.com/ShekhMahmud.24
+ise-kzhh-iwr
